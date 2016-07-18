c2c22fb29890cf6fe8edba9ba9d91d2e7fcdc986
Henry <rynehx@gmail.com>
2016-07-18 14:27:33 -0700

    fixing gallery flux loop

--- frontend/util/galleryApiUtils.js
+++ frontend/util/galleryApiUtils.js
@@ -3,8 +3,9 @@ var GalleryConstants = require('../constants/galleryConstants');
 
 var GalleryApiUtils = {
   fetchUserGalleries: function(user){
-      console.log(user)
+
       _500px.api('users/' + user.id + '/galleries',{rpp: 100, sort: 'last_added_to_at', include_cover: 1}, function (response) {
+        console.log()
           Dispatcher.dispatch({
             actionType: GalleryConstants.fetchUserGalleries,
             items: response.data.galleries
