a82ef0b0afb081cfe61e48b8968f8873e3793f08
Henry <rynehx@gmail.com>
2016-07-18 03:53:32 -0700

    changed from dislike to unlike

--- frontend/util/photosApiUtils.js
+++ frontend/util/photosApiUtils.js
@@ -21,7 +21,7 @@ module.exports = {
   },
 
   unlikePhoto: function(photo, imageSize){
-    _500px.api('/photos/' + photo.id + '/vote',"post", {id: photo.id, vote:0}, function(res){
+    _500px.api('/photos/' + photo.id + '/vote',"delete", {id: photo.id}, function(res){
       console.log(res)
       if(res.success){
         console.log(res)
